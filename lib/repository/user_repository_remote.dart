417d777f56eb3e6a658c135f025fb40a74837757
EduKaique <eduardoalberico18@gmail.com>
2026-01-13 09:33:45 -0300

    feat finalizando UserRepository

--- lib/repository/user_repository_remote.dart
+++ lib/repository/user_repository_remote.dart
@@ -5,31 +5,49 @@ import 'package:flowcase/services/api_dummyjson.dart';
 import 'package:flowcase/utils/result.dart';
 
 class UserRepositoryRemote implements UserRepository {
-
   final ApiDummyjson _api;
 
-  UserRepositoryRemote({ required ApiDummyjson api }) : _api = api;
+  UserRepositoryRemote({required ApiDummyjson api}) : _api = api;
 
   @override
-  Future<Result<List<User>>> getUsers() {
-    
-    // final result = await _api.getUsers();
+  Future<Result<List<User>>> getUsers() async {
+    final result = await _api.getUsers();
 
-    // switch (result) {
-    //   case Ok<List<UserApiModel>>():
-    //     final domainUsers = result.value.map((apiModel) {
-    //       return User(id: apiModel.id, fullName: '${apiModel.firstName} ${apiModel.lastName}', email: apiModel.email, imageUrl: apiModel.image),
-    //     }).toList();
+    switch (result) {
+      case Ok<List<UserApiModel>>():
+        final domainUsers = result.value.map((apiModel) {
+          return User(
+            id: apiModel.id,
+            fullName: '${apiModel.firstName} ${apiModel.lastName}',
+            email: apiModel.email,
+            imageUrl: apiModel.image,
+          );
+        }).toList();
 
-    //   case Error():
+        return Result.ok(domainUsers);
 
-    // }
-     throw UnimplementedError();
+      case Error():
+        return Result.error(result.error);
+    }
   }
 
   @override
-  Future<Result<User>> getUser(int id) {
-    // TODO: implement getUser
-    throw UnimplementedError();
+  Future<Result<User>> getUser(int id) async {
+    final result = await _api.getUserById(id);
+
+    switch (result) {
+      case Ok<UserApiModel>():
+        final apiModel = result.value;
+        return Result.ok(
+          User(
+            id: apiModel.id,
+            fullName: '${apiModel.firstName} ${apiModel.lastName}',
+            email: apiModel.email,
+            imageUrl: apiModel.image,
+          ),
+        );
+      case Error():
+        return Result.error(result.error);
+    }
   }
 }

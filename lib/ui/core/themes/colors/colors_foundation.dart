ac7ff1a617de352344a9ed45f63b8a1c40d19181
EduKaique <eduardoalberico18@gmail.com>
2026-02-11 17:14:30 -0300

    feat add theme with dark/light color schemes

---
+++ lib/ui/core/themes/colors/colors_foundation.dart
@@ -0,0 +1,135 @@
+import 'package:flutter/material.dart';
+
+abstract final class AppColorsFoundation {
+
+  // mintGreen
+  static const Color mintGreen100 = Color(0xFFFFFFFF);
+  static const Color mintGreen99 = Color(0xFFF6FFF9);
+  static const Color mintGreen95 = Color(0xFFC2FFDC);
+  static const Color mintGreen90 = Color(0xFF7BF8BB);
+  static const Color mintGreen80 = Color(0xFF5DDB9E);
+  static const Color mintGreen78 = Color(0xFF41CF91);
+  static const Color mintGreen70 = Color(0xFF22C382);
+  static const Color mintGreen60 = Color(0xFF00A66D);
+  static const Color mintGreen50 = Color(0xFF008959);
+  static const Color mintGreen40 = Color(0xFF006D46);
+  static const Color mintGreen30 = Color(0xFF005234);
+  static const Color mintGreen20 = Color(0xFF003822);
+  static const Color mintGreen10 = Color(0xFF002112);
+  static const Color mintGreen5 = Color(0xFF001109);
+  static const Color mintGreen0 = Color(0xFF000000);
+
+  // navyBlue
+  static const Color navyBlue100 = Color(0xFFFFFFFF);
+  static const Color navyBlue99 = Color(0xFFFFFBFF);
+  static const Color navyBlue95 = Color(0xFFEEF2FF);
+  static const Color navyBlue90 = Color(0xFFDDE5FF);
+  static const Color navyBlue80 = Color(0xFFBFCBEB);
+  static const Color navyBlue70 = Color(0xFFA3AECD);
+  static const Color navyBlue60 = Color(0xFF8893B1);
+  static const Color navyBlue50 = Color(0xFF6D7996);
+  static const Color navyBlue40 = Color(0xFF54607C);
+  static const Color navyBlue30 = Color(0xFF3C4863);
+  static const Color navyBlue20 = Color(0xFF243049);
+  static const Color navyBlue15 = Color(0xFF17253C);
+  static const Color navyBlue10 = Color(0xFF0D1522);
+  static const Color navyBlue0 = Color(0xFF000000);
+
+  // neutral
+  static const Color neutral100 = Color(0xFFFFFFFF);
+  static const Color neutral99 = Color(0xFFFDFBFF);
+  static const Color neutral98 = Color(0xFFF8F9FF);
+  static const Color neutral96 = Color(0xFFF2F3FD);
+  static const Color neutral95 = Color(0xFFEFF0FA);
+  static const Color neutral94 = Color(0xFFECEDF7);
+  static const Color neutral92 = Color(0xFFE7E8F2);
+  static const Color neutral90 = Color(0xFFE1E2EC);
+  static const Color neutral87 = Color(0xFFD9DBE5);
+  static const Color neutral80 = Color(0xFFC5C7D1);
+  static const Color neutral70 = Color(0xFFA9ACB5);
+  static const Color neutral60 = Color(0xFF8F919A);
+  static const Color neutral50 = Color(0xFF757880);
+  static const Color neutral40 = Color(0xFF5C5F67);
+  static const Color neutral30 = Color(0xFF44474E);
+  static const Color neutral24 = Color(0xFF373A40);
+  static const Color neutral22 = Color(0xFF32353C);
+  static const Color neutral20 = Color(0xFF2E3137);
+  static const Color neutral17 = Color(0xFF282A31);
+  static const Color neutral12 = Color(0xFF1D2026);
+  static const Color neutral10 = Color(0xFF191C22);
+  static const Color neutral6 = Color(0xFF0E1015);
+  static const Color neutral4 = Color(0xFF080B10);
+  static const Color neutral0 = Color(0xFF000000);
+
+  // neutralVariant
+  static const Color neutralVariant100 = Color(0xFFFFFFFF);
+  static const Color neutralVariant99 = Color(0xFFFFFBFF);
+  static const Color neutralVariant95 = Color(0xFFF2F3FF);
+  static const Color neutralVariant90 = Color(0xFFE4E4F1);
+  static const Color neutralVariant80 = Color(0xFFC7C7D3);
+  static const Color neutralVariant70 = Color(0xFFACACB8);
+  static const Color neutralVariant60 = Color(0xFF91919D);
+  static const Color neutralVariant50 = Color(0xFF777782);
+  static const Color neutralVariant40 = Color(0xFF5E5E69);
+  static const Color neutralVariant30 = Color(0xFF464751);
+  static const Color neutralVariant20 = Color(0xFF30303C);
+  static const Color neutralVariant10 = Color(0xFF1B1B26);
+  static const Color neutralVariant0 = Color(0xFF000000);
+
+  // crimsonRed
+  static const Color crimsonRed100 = Color(0xFFFFFFFF);
+  static const Color crimsonRed95 = Color(0xFFFFEDEA);
+  static const Color crimsonRed90 = Color(0xFFFFDAD6);
+  static const Color crimsonRed80 = Color(0xFFFFB4AB);
+  static const Color crimsonRed70 = Color(0xFFFF897D);
+  static const Color crimsonRed60 = Color(0xFFFF5449);
+  static const Color crimsonRed50 = Color(0xFFDE3730);
+  static const Color crimsonRed40 = Color(0xFFBA1A1A);
+  static const Color crimsonRed30 = Color(0xFF93000A);
+  static const Color crimsonRed20 = Color(0xFF690005);
+  static const Color crimsonRed10 = Color(0xFF410002);
+
+  // amberYellow
+  static const Color amberYellow100 = Color(0xFFFFFFFF);
+  static const Color amberYellow95 = Color(0xFFFFEEE1);
+  static const Color amberYellow90 = Color(0xFFFFDDB3);
+  static const Color amberYellow80 = Color(0xFFFFB860);
+  static const Color amberYellow70 = Color(0xFFFF9C08);
+  static const Color amberYellow60 = Color(0xFFC77F00);
+  static const Color amberYellow50 = Color(0xFFA66800);
+  static const Color amberYellow40 = Color(0xFF865200);
+  static const Color amberYellow30 = Color(0xFF663D00);
+  static const Color amberYellow20 = Color(0xFF482900);
+  static const Color amberYellow10 = Color(0xFF291800);
+
+  // emeraldGreen
+  static const Color emeraldGreen100 = Color(0xFFFFFFFF);
+  static const Color emeraldGreen99 = Color(0xFFF7FFF9);
+  static const Color emeraldGreen95 = Color(0xFFE8FFF0);
+  static const Color emeraldGreen90 = Color(0xFFC2FFDC);
+  static const Color emeraldGreen80 = Color(0xFF92F5B1);
+  static const Color emeraldGreen70 = Color(0xFF76E091);
+  static const Color emeraldGreen60 = Color(0xFF56C576);
+  static const Color emeraldGreen50 = Color(0xFF37A95D);
+  static const Color emeraldGreen40 = Color(0xFF148F45);
+  static const Color emeraldGreen30 = Color(0xFF005328);
+  static const Color emeraldGreen20 = Color(0xFF00391C);
+  static const Color emeraldGreen10 = Color(0xFF00210E);
+  static const Color emeraldGreen0 = Color(0xFF000000);
+
+  // azureBlue
+  static const Color azureBlue100 = Color(0xFFFFFFFF);
+  static const Color azureBlue99 = Color(0xFFFDFBFF);
+  static const Color azureBlue95 = Color(0xFFEFF4FF);
+  static const Color azureBlue90 = Color(0xFFD7E3FF);
+  static const Color azureBlue80 = Color(0xFFB0C9FF);
+  static const Color azureBlue70 = Color(0xFF85ACFF);
+  static const Color azureBlue60 = Color(0xFF5291FF);
+  static const Color azureBlue50 = Color(0xFF2676E0);
+  static const Color azureBlue40 = Color(0xFF004A88);
+  static const Color azureBlue30 = Color(0xFF00335D);
+  static const Color azureBlue20 = Color(0xFF001D36);
+  static const Color azureBlue10 = Color(0xFF000000);
+
+  
+}
